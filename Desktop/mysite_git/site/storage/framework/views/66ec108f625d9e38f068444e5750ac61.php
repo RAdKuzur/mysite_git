9206e7cfbcdae9107a0dff6a91d2bd5c3fc0c489
RAdKuzur <drive16052003@gmail.com>
2023-11-23 19:49:09 +0400

    19:48 23/11/2023

---
+++ Desktop/mysite_git/site/storage/framework/views/66ec108f625d9e38f068444e5750ac61.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" type="text/css" href="../css/bootstrap.css">
+    <title>Главная</title>
+</head>
+<body>
+    <div align="center"> Вход в учётную запись произведён успешно </div>
+</body>
+</html><?php /**PATH C:\Users\Hp\Desktop\OpenServer\domains\localhost\site\example-app\resources\views/main.blade.php ENDPATH**/ ?>
---
+++ Desktop/mysite_git/site/storage/framework/views/7c450ad1f662dffcbecc0a385a24818a.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel = "stylesheet" type="text/css" href="../css/register.css">
+    <link rel = "stylesheet" type="text/css" href="../css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вход</title>
+</head>
+<body>
+    <div class = "text-1"><h1>Окно регистрации</h1></div>
+    <div class = "container">
+        <div class = "form-1">
+            <form action="/main" method = "POST">
+                <?php echo csrf_field(); ?>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Your Name</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Your nickname</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Nickname">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div> 
+                <div class="d-grid gap-2 d-md-block">
+                    <button class="btn btn-danger" type="submit">Зарегистрироваться</button>
+                </div>
+            </form>
+       
+        </div>
+    </div>
+    <div class = "bottom-1"> 
+    </div>
+</body>
+</html><?php /**PATH C:\Users\Hp\Desktop\OpenServer\domains\localhost\site\example-app\resources\views/register.blade.php ENDPATH**/ ?>
