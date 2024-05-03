d25e0df31cb97ee7739035d582c576af3510162e
PR P. <Praktik@RST.EDU>
2024-05-03 16:06:39 +0400

    03/05/2024 16:06

--- site_yii/commands/HelloController.php
+++ site_yii/commands/HelloController.php
@@ -6,16 +6,19 @@
  */
 
 namespace app\commands;
+use app\controllers\DocsOutController;
+use app\models\common\People;
+use app\models\common\SendMethod;
 use Yii;
 use app\models\common\DocumentOut;
 use app\models\components\Logger;
-use app\models\work\DocumentOutWork;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use yii\web\UploadedFile;
-
+use app\commands;
 class HelloController extends Controller
 {
+    /**
     /**
      * This command echoes what you have entered as the message.
      * @param string $message the message to be echoed.
@@ -32,38 +35,34 @@ class HelloController extends Controller
     {
         return ['m' => 'message', 'd' => 'message2'];
     }
-    public function actionIndex($message = ' ', $message2 = ' ')
+    public function actionIndex()
     {
-        $model = new DocumentOutWork();
-        $model->document_name = "default";
 
-        if ($model->load(Yii::$app->request->post())) {
-            $model->applications = '';
-            $model->doc = '';
-            $model->getDocumentNumber();
-            $model->Scan = '';
+        $model = new TestDocumentOutWork();
+        $model->document_number = 1;
+        $model->document_postfix = null;
+        $model->document_date = date("Y-m-d");
+        $model->document_name = "Test Document Name";
+        $model->document_theme = "Test Document Theme";
+        $model->correspondent_id = 1;
+        $model->company_id = 1;
+        $model->position_id = 1;
+        $model->signed_id = 1;
+        $model->executor_id = 1;
+        $model->send_method_id = 2;
+        $model->sent_date = date("Y-m-d");
+        $model->Scan = 'Test Scan';
+        $model->doc = 'Test Document';
+        $model->applications = 'Test Applications';
+        $model->creator_id = 1;
+        $model->last_edit_id = 1;
+        $model->key_words = 'Test Key Words';
 
-            $model->creator_id = Yii::$app->user->identity->getId();
-            $model->scanFile = UploadedFile::getInstance($model, 'scanFile');
-            $model->applicationFiles = UploadedFile::getInstances($model, 'applicationFiles');
-            $model->docFiles = UploadedFile::getInstances($model, 'docFiles');
-
-
-            if ($model->validate(false)) {
-                if ($model->scanFile != null)
-                    $model->uploadScanFile();
-                if ($model->applicationFiles != null)
-                    $model->uploadApplicationFiles();
-                if ($model->docFiles != null)
-                    $model->uploadDocFiles();
-                $model->save(false);
-                Logger::WriteLog(Yii::$app->user->identity->getId(), 'Добавлен исходящий документ ' . $model->document_theme);
-
-            }
-            echo $this->message . $this->message2 . "\n";
-
-
-            return ExitCode::OK;
+        if($model->save(false)) {
+            echo 'Запись добавлена' . "\n";
+        }
+        else {
+            echo 'Запись не добавлена' . "\n";
         }
     }
 }
